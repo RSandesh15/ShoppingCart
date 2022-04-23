a742753deb84cb54f4d333311329e922bc3e8d90
RSandesh15 <fannyvalette45@gmail.com>
2022-04-23 18:39:59 -0400

    button

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,10 +2,23 @@ import React from "react";
 
 
 
-export default function Basket(prop){
+export default function Basket(props){
+    const {cartItems, onAdd, onRemove} = props;
     return (
     <aside className="block col-1">
         <h2>Cart Items</h2>
+        <div>
+            {cartItems.length === 0 && <div> Cart Is Empty </div>}
+            {cartItems.map((item)=> (
+                    <div key = {item.id} className = "row">
+                        <div>{item.name}</div>
+                        <div>
+                            <button onClick = {()=>onAdd(item)} className="add">+</button>
+                            <button onClick = {()=>onRemove(item)} className="remove">-</button>
+                        </div>
+                    </div>
+            ))}
+        </div>
     </aside>
     );
 }
