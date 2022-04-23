bfd3ed050a6ccbf7efea768ca636fedde9065b7b
RSandesh15 <fannyvalette45@gmail.com>
2022-04-23 17:32:52 -0400

    cart_initial

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+export default function Header(prop){
+    return (
+        <header className="row block center">
+            <div>
+                <a href="#/">
+                    <h1> Small Shopping Cart</h1>
+                </a>
+            </div>
+            <div>
+                <a href="#/cart">Cart </a>
+            </div>
+        </header>
+
+    );
+}
